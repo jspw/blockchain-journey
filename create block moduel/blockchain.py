f69ace26d4d0a75205841c0a0f409cf7d619728d
jspw <mhshifat757@gmail.com>
2022-01-14 02:42:56 +0600

    finish creating and testing a demo blockchain with flask web app api

--- create block moduel/blockchain.py
+++ create block moduel/blockchain.py
@@ -16,10 +16,7 @@ import json
 from flask import Flask,jsonify
 
 
-
 # building blockchain 
-
-
 class Blockhain:
     def __init__(self):
         self.chain=[]
@@ -68,10 +65,63 @@ class Blockhain:
                 return False
             previous_block = block
             block_index +=1
+        return True
         
         
         
+# creating a Web application
+app = Flask(__name__)
+    
+    
+# creating a blockchain
+blockchain =  Blockhain()
 
 
 
-# mining our blockchain
+# mining a new blockchain
+@app.route("/mine_block",methods=['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof= blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof,previous_hash)
+    response = {'message' : 'Congratulations you just mined a block!',
+                'index':block['index'],
+                'timestamp' : block['timestamp'],
+                'proof' : block['proof'],
+                'previous_hash' : block['previous_hash']
+                }
+    
+    return jsonify(response),201
+    
+    
+# getting the full blockchain
+@app.route('/get_chain',methods=['GET'])
+def get_chain():
+    response = {
+        'chain' : blockchain.chain,
+        'length' : len(blockchain.chain)
+        }
+    
+    return jsonify(response),200
+
+
+### check if the chain is valid
+@app.route("/is_chain_valid",methods=["GET"])
+def check_if_chain_valid():
+    chain = blockchain.chain
+    is_valid = blockchain.is_chain_valid(chain)
+    return jsonify({
+        "is chain valid" : is_valid
+        }),200
+    
+
+
+# running the app
+app.run(host='localhost',port=8081)
+
+
+
+
+
